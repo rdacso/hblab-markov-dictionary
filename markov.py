3a24e398f1c9d9369c1b062e162b6a109d49ed00
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-05 12:14:42 -0700

    completed make_text function

--- markov.py
+++ markov.py
@@ -7,18 +7,11 @@ def open_and_read_file(file_path):
     Takes a string that is a file path, opens the file, and turns
     the file's contents as one string of text.
     """
-
-    # your code goes here
+    # Opens file and reads it line by line. Turns it into a string.
     content = open(file_path).read()
-    words = content.split()
-    text = ""
-    for word in words:
-        text += word + " "
 
-    # print text
 
-    return text
-    #"This should be a variable that contains your file text as one long string"
+    return content
 
 def make_chains(text_string):
     """Takes input text as string; returns _dictionary_ of markov chains.
@@ -35,51 +28,49 @@ def make_chains(text_string):
 
     chains = {}
 
-    # your code goes here
 
     words = text_string.split()
     # for item in the length of words - 2  
-    for i in range(len(words) - 2):
-        # adding the words 1 and 2 to the dictionary as keys 
-        # and the third word as a value
-        # conditional for whether this tuple already exists in chain
-    
-        if (words[i], words[i + 1]) in chains:
-            # this is true
-            value = chains[(words[i], words[i + 1])]
-            value.append(words[i + 2])
+    for i in range(len(words) - 2): 
+        # conditional to check to see if tuple pair is not in chains dictionary
+        if (words[i], words[i + 1]) not in chains:
+            # assign the key value to an empty list
+            chains[words[i], words[i + 1]] = []
+        # appends value to the list of values in the dictionary
+        chains[(words[i], words[i + 1])].append(words[i + 2])
 
-        else:
-            chains[words[i], words[i + 1]] = [words[i + 2]]
     return chains
 
 
 def make_text(chains):
     """Takes dictionary of markov chains; returns random text."""
-
+    # empty string to store results
     text = ""
+    # variable to store randomly generated key
     random_key = choice(chains.keys())
-    # if the random key generated exists in the dictionary:
+    # while loop to iterate through dictionary until condiiton is met
     while random_key in chains:
         # add the random key generated to the text string
-        text += random_key[0] + " " + random_key[1]
+        text += random_key[0] + " " + random_key[1] + " "
         # generate a random value based off of random key
         value = choice(chains.get(random_key))
         # combine second key value with random word for new key pair
-        new_random = random_key[1] + value
+        random_key = (random_key[1], value)
+    # add the last word to the text string
+    text += random_key[1]
 
     return text
 
 
 
 input_path = "green-eggs.txt"
-# print input_path
+
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
-# print input_text
+
 # Get a Markov chain
 chains = make_chains(input_text)
-# print chains
+
 
 #Produce random text
 random_text = make_text(chains)
